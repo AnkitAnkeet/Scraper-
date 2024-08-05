b6d30596ef29427dc4ba08eee0f4eca71e50b6b0
GOD <ankitsahoo740@gmail.com>
2024-08-05 18:48:18 +0530

    ME_QA_XSCRAPE_DATA_MODULE_L1

--- src/test/java/demo/TestCases.java
+++ src/test/java/demo/TestCases.java
@@ -6,16 +6,26 @@ import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.logging.LogType;
 import org.openqa.selenium.logging.LoggingPreferences;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.logging.Level;
 // import io.github.bonigarcia.wdm.WebDriverManager;
 import demo.wrappers.Wrappers;
 
 public class TestCases {
-    ChromeDriver driver;
+    static ChromeDriver driver;
 
     /*
      * TODO: Write your tests here with testng @Test annotation. 
@@ -47,13 +57,121 @@ public class TestCases {
         driver = new ChromeDriver(options);
 
         driver.manage().window().maximize();
+       
+        System.out.println("Start Automation: Create a Scraper");
     }
 
     @AfterTest
     public void endTest()
-    {
+    {  
+        System.out.println("End Automation: Create a Scraper");
         driver.close();
         driver.quit();
+    }
 
+    @Test
+    public static void testCase01(){
+        System.out.println("Start Test Case: testCase01");
+
+        //launch the url 
+        driver.get("https://www.scrapethissite.com/pages/");
+
+        //apply an assert whether the correct url is launched or not
+        Assert.assertTrue(driver.getCurrentUrl().contains("scrape") && driver.getCurrentUrl().contains("pages"),"The current url is not same as the url given");
+
+        //click on the "Hockey Teams: Forms, Searching and Pagination" link
+        Wrappers.clickPage(driver, "Hockey Teams: Forms, Searching and Pagination");
+        Assert.assertTrue(driver.getCurrentUrl().toLowerCase().contains("forms"),"Did not click to correct url");
+
+        //wait for the page to load completely  
+        Wrappers.waitFor(driver, By.xpath("//table//tr[26]"));
+
+        //scroll to the end of the table to load all elements
+        Wrappers.scrollTo(By.xpath("//table//tr[26]"), driver);
+
+        //fetch the details and collect the Team Name, Year and Win % for the teams with Win % less than 40% (0.40) for page 1,2,3,4 
+        List<String> sortedList = Wrappers.fetchHockeyTable(driver, new int[] {1,2,3,4} , new String[] {"Team Name", "Year", "Win %"},"Win %","<", 0.4);
+    
+
+        //populating a map from sortedList list and making a list 
+        List<Map<String, Object>> finalList = new ArrayList<>();
+
+        for(int i=0;i<=sortedList.size()-3;i+=3){
+        Map<String, Object> inputMap = new HashMap<String, Object>();
+        inputMap.put("Epoch Time of Scrape",System.currentTimeMillis()/1000);
+        inputMap.put("Team Name",sortedList.get(i));
+        inputMap.put("Year",sortedList.get(i+1));
+        inputMap.put("Win %",sortedList.get(i+2));
+        finalList.add(inputMap);
+        }
+        
+        ObjectMapper mapper = new ObjectMapper();
+         //converting list to a JSON as string
+        try {
+            String hockeyFile = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(finalList);
+            System.out.println(hockeyFile);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        String userDir = System.getProperty("user.dir");
+        //writing JSON on a file
+        try {
+            mapper.writerWithDefaultPrettyPrinter().writeValue(new File(userDir + "\\src\\test\\resources\\hockey-team-data.json"), finalList);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("End Test Case: testCase01");
+    }
+
+
+
+    @Test
+    public static void testCase02(){
+        System.out.println("Start Test Case: testCase02");
+
+        //launch the url 
+        driver.get("https://www.scrapethissite.com/pages/");
+
+        //apply an assert whether the correct url is launched or not
+        Assert.assertTrue(driver.getCurrentUrl().contains("scrape") && driver.getCurrentUrl().contains("pages"),"The current url is not same as the url given");
+
+        //click on the "Oscar Winning Films: AJAX and Javascript" link
+        Wrappers.clickPage(driver, "Oscar Winning Films: AJAX and Javascript");
+
+        //click on any year . let 2015
+        ArrayList<String> sortedList = Wrappers.fetchFilmsTable(driver, new int[] {2015,2014,2013,2012,2011,2010}, new int[] {1,2,3,4,5}, new String[]{"Title","Nominations","Awards","Best Picture"});
+
+
+        List<Map<String, Object>> finalList = new ArrayList<>();
+
+        for(int i=0;i<=sortedList.size()-5;i+=5){
+        Map<String, Object> inputMap = new HashMap<String, Object>();
+        inputMap.put("Epoch Time of Scrape",System.currentTimeMillis()/1000);
+        inputMap.put("Year",sortedList.get(i));
+        inputMap.put("Title",sortedList.get(i+1));
+        inputMap.put("Nominations",sortedList.get(i+2));
+        inputMap.put("Awards",sortedList.get(i+3));
+        inputMap.put("Best Pictures",sortedList.get(i+4));
+        finalList.add(inputMap);
+        }
+
+        ObjectMapper mapper = new ObjectMapper();
+        //converting list to a JSON as string
+       try {
+           String oscarFile = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(finalList);
+           System.out.println(oscarFile);
+       } catch (JsonProcessingException e) {
+           e.printStackTrace();
+       }
+       String userDir = System.getProperty("user.dir");
+       //writing JSON on a file
+       try {
+           mapper.writerWithDefaultPrettyPrinter().writeValue(new File(userDir + "\\src\\test\\resources\\oscar-winner-data.json"), finalList);
+       } catch (IOException e) {
+           e.printStackTrace();
+       }
+
+        System.out.println("End Test Case: testCase02");
     }
 }
--- src/test/java/demo/wrappers/Wrappers.java
+++ src/test/java/demo/wrappers/Wrappers.java
@@ -1,19 +1,232 @@
 package demo.wrappers;
 
-import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
-import org.openqa.selenium.Alert;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
 import java.time.Duration;
 
+
+
 public class Wrappers {
-    /*
-     * Write your selenium wrappers here
-     */
+
+
+
+    //method to click any page with its text title
+   public static void clickPage(ChromeDriver driver, String pageTitle){
+        List<WebElement> pageLinks = driver.findElements(By.xpath("//div[@class='page']/h3"));
+        
+        for(WebElement pageLink : pageLinks){
+            try{
+                if(pageLink.getText().equals(pageTitle)){
+                    pageLink.click();
+                    break;//exit the loop after clicking the desired page link
+                }
+            }catch(StaleElementReferenceException e){
+                //re-locate the elements and retry if a StaleElementReferenceException occurs
+                pageLinks = driver.findElements(By.xpath("//div[@class='page']/h3"));
+            }catch(Exception e){
+        
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    //method to wait for an element 
+    public static void waitFor(ChromeDriver driver, By locator){
+
+         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+           wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+
+    //method for javascript executor to scroll
+    public static void scrollTo(By locator,ChromeDriver driver){
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].scrollIntoView(true);",driver.findElement(locator));
+    }
+
+
+
+    //method to fetch the details of the table with filters 
+    public static List<String> fetchHockeyTable(ChromeDriver driver, int[] pageNo, String[] tableHeadtitles, String conditionOn, String condition, double targetValue){
+        List<String> sortedList = new ArrayList<>();
+        
+
+        try{
+            //click on the desired page numbers
+            for(int i = 0; i <= pageNo.length - 1; i++){
+                WebElement footerPageNo = driver.findElement(By.xpath("//a[@href='/pages/forms/?page_num="+pageNo[i]+"']"));
+                        footerPageNo.click();
+    
+                //wait for the page to load and scroll to the end to ensure the table is loaded
+                waitFor(driver, By.xpath("//table//tr[26]"));
+                scrollTo(By.xpath("//table//tr[26]"), driver);
+    
+                //initialize column indices
+                int[] columnIndices = new int[tableHeadtitles.length];
+    
+                //listing all headings  
+                List<WebElement> headings = driver.findElements(By.xpath("//table//th"));
+    
+                for(int j = 0; j <= tableHeadtitles.length - 1; j++){
+                    for(WebElement heading : headings){
+                        if(heading.getText().equals(tableHeadtitles[j])){
+                            columnIndices[j] = headings.indexOf(heading) + 1; //adding values to indices array
+                        }
+                    }
+                }
+    
+                //getting the respective int element from indices for string tableHeadTitle
+                int k = columnIndices[Arrays.asList(tableHeadtitles).indexOf(conditionOn)];
+    
+                //getting the webelements acc to table headings
+                List<WebElement> elements = driver.findElements(By.xpath("//table//tr/td[" + k + "]"));
+    
+                List<WebElement> filteredElements = new ArrayList<>();
+    
+                for(WebElement filteredElement : elements){
+                    //making it generic for each condition
+                    try{
+                        switch(condition){
+                            case "<":
+                                if(Double.parseDouble(filteredElement.getText().trim()) < targetValue){
+                                    filteredElements.add(filteredElement);
+                                }
+                                break; //exit the loop of condition operators
+                            case ">":
+                                if(Double.parseDouble(filteredElement.getText().trim()) > targetValue){
+                                    filteredElements.add(filteredElement);
+                                }
+                                break; //exit the loop of condition operators
+                            case "=":
+                                if(Double.parseDouble(filteredElement.getText().trim()) == targetValue){
+                                    filteredElements.add(filteredElement);
+                                }
+                                break; //exit the loop of condition operators
+                            default:
+                                System.out.println("Check your condition");
+                        }
+                    }catch(NumberFormatException e){
+                        System.out.println("Error parsing number: " + filteredElement.getText());
+                    }
+                }
+    
+                //initialize row indices
+                int[] rowIndices = new int[filteredElements.size()];
+    
+                //add values into it
+                for(int p = 0; p <= filteredElements.size() - 1; p++){
+                    rowIndices[p] = elements.indexOf(filteredElements.get(p)) + 2;
+                }
+    
+                //get the final table in arraylist
+                for(int m = 0; m <= rowIndices.length - 1; m++){
+                    for(int n = 0; n <= columnIndices.length - 1; n++){
+                        try{
+                            sortedList.add(driver.findElement(By.xpath("//table//tr[" + rowIndices[m] + "]/td[" + columnIndices[n] + "]")).getText().trim());
+                            
+                        }catch (Exception e){
+                            System.out.println("Error retrieving table data at row " + rowIndices[m] + " and column " + columnIndices[n]);
+                        }
+                    }
+                }
+            }
+        }catch(Exception e){
+            System.out.println("An error occurred while fetching the table: " + e.getMessage());
+        }
+        return sortedList;
+        
+    }
+
+
+
+
+
+    public static ArrayList<String> fetchFilmsTable(ChromeDriver driver, int[] years, int[] indicesOfFilms, String[] headingsList){
+        ArrayList<String> sortedList = new ArrayList<>();
+        
+        for(int i = 0; i <= years.length - 1; i++){
+            try{
+                //to iterate over all years
+                WebElement year = driver.findElement(By.xpath("//a[@id='" + years[i] + "']"));
+                year.click();
+    
+                //to ensure the page is loaded properly with all displayed elements
+                waitFor(driver, By.xpath("//table//tr[5]"));
+                scrollTo(By.xpath("//table//tr[5]"), driver);
+    
+                //initializing a column array based on given 
+                int[] columnIndices = new int[headingsList.length - 1];
+                List<WebElement> headings = driver.findElements(By.xpath("//table//th"));
+    
+                //iterate for every string except the "Best Picture"
+                for(int j = 0; j <= headingsList.length - 1; j++){
+                    boolean execute = false;
+                    for(WebElement heading : headings){
+                        if(heading.getText().trim().equals("Best Picture")){
+                            continue;
+                        }
+                        if(!execute && heading.getText().trim().equals(headingsList[j])){
+                            columnIndices[j] = headings.indexOf(heading) + 1;
+                            execute = true;
+                        }
+                    }
+                }
+    
+                //listing the table data in string format
+                for(int m = 0; m <= indicesOfFilms.length - 1; m++){
+                    sortedList.add(years[i]+"");
+                    for(int n = 0; n <= columnIndices.length - 1; n++){
+                        try{
+                            String detail = driver.findElement(By.xpath("//table/tbody/tr[" + (indicesOfFilms[m]) + "]/td[" + columnIndices[n] + "]")).getText().trim();
+                            sortedList.add(detail);
+                        }catch(Exception e){
+                            System.out.println("Error retrieving detail for row " + indicesOfFilms[m] + " and column " + columnIndices[n] + ": " + e.getMessage());
+                        }
+                    }
+                    sortedList.add("Not winner");
+                }
+    
+                //adding winner for winner films
+                List<WebElement> marks = driver.findElements(By.xpath("//table/tbody/tr/td/i"));
+                for(WebElement mark : marks){
+                    if(mark.isDisplayed()){
+                        WebElement winnerRow = mark.findElement(By.xpath("./ancestor::tr"));
+                        int winnerIndex = driver.findElements(By.xpath("//table/tbody/tr")).indexOf(winnerRow) + 2;
+                        for(int x = 0; x < sortedList.size(); x += headingsList.length+1) {
+                            if(sortedList.get(x+1).contains(winnerRow.findElement(By.xpath("td["+columnIndices[0]+"]")).getText().trim())){
+                                sortedList.set(x+headingsList.length, "Winner");
+                            }
+                        }
+                    }
+                }
+            }catch(Exception e){
+                System.out.println("Error processing year " + years[i] + ": " + e.getMessage());
+            }
+        }
+        return sortedList;
+    }
+    
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
